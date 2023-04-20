7647e04a261df210a35af76b8cfde1ffb403372e
VadimMiropolskyi <wadim.mir0602@gmail.com>
2023-04-20 15:00:37 +0200

    start page 'about', connect scss to page 'about'

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/', to: 'home#index'
   get '/home', to: 'home#index'
   get '/about', to: 'about#index'
   get '/contact', to: 'contact#index'
